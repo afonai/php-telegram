26b580db98814dd734a70728bb753242021d800c
afonai <ivanivanov9564tt@gmail.com>
2023-11-30 17:47:25 +0400

    писал формат даты в разных поясах

--- test.php
+++ test.php
@@ -3,20 +3,10 @@ require_once('vendor/autoload.php');
 
 use afonai\telegram\Telegram;
 
-$telegram = new Telegram('6434974241:AAExW6vyXr39ubBsJxIvS3MapVxy9wBIEHQ');
-$telegram->sendMessage('1227307765', 'Привет мир!' . strtotime("+1 day"), [
-    [
-        [
-            'text' => date('c', time()+14400),
-            'url' => 'https://www.php.net/manual/ru/function.json-encode.php',
-        ],
-    ],
-    [
-        [
-            'text' => date('Y-m-d в H:i:s', strtotime("next Monday")),
-            'url' => 'https://www.php.net/manual/ru/function.json-encode.php',
-        ],
-    ]
-]);
+$time = 1701349999;
 
-print_r($telegram);
+/*$telegram = new Telegram('6434974241:AAExW6vyXr39ubBsJxIvS3MapVxy9wBIEHQ');
+$telegram->sendMessage('1227307765', 'Привет мир!' . );*/
+date_default_timezone_set('Pacific/Easter');
+
+print_r(date('r', $time));
