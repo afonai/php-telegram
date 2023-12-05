5853b722ce7ca1418e0eba0683d6bfc86ec97721
afonai <ivanivanov9564tt@gmail.com>
2023-12-05 17:55:18 +0400

    прибавлял время unix

--- test.php
+++ test.php
@@ -4,9 +4,10 @@ require_once('vendor/autoload.php');
 use afonai\telegram\Telegram;
 
 $time = 1701349999;
-
+$day = 100;
 /*$telegram = new Telegram('6434974241:AAExW6vyXr39ubBsJxIvS3MapVxy9wBIEHQ');
 $telegram->sendMessage('1227307765', 'Привет мир!' . );*/
-date_default_timezone_set('Pacific/Easter');
 
-print_r(date('r', $time));
+
+
+print_r($time + 172800);
