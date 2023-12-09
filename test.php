13f17bfaec4b281427b4cd3e27ecb05a5e6635ff
afonai <ivanivanov9564tt@gmail.com>
2023-12-09 17:53:53 +0400

    работал с циклами

--- test.php
+++ test.php
@@ -1,13 +1,11 @@
 <?php
 require_once('vendor/autoload.php');
 
-use afonai\telegram\Telegram;
+$test = [];
 
-$time = 1701349999;
-$day = 100;
-/*$telegram = new Telegram('6434974241:AAExW6vyXr39ubBsJxIvS3MapVxy9wBIEHQ');
-$telegram->sendMessage('1227307765', 'Привет мир!' . );*/
+for ($i = 0; $i <= 10; $i++) {
+    $time = time() + $i;
+    $test[$time] = date('c', $time);
+}
 
-
-
-print_r($time + 172800);
+print_r($test);
